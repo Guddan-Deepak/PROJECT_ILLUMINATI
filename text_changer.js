3479c9e265e9f837bbac39da50a28f02259ca153
TEJAS MISHRA <tejasmishra040907@gmail.com>
2025-02-02 22:28:48 +0530

    Add files via upload

---
+++ text_changer.js
@@ -0,0 +1,18 @@
+const typed = new Typed('.text-changing',  {
+    strings: [
+        'Beach Gateways', 
+        'Mountain Retreats', 
+        'Cultural Hotspots', 
+        'Historical Landmarks', 
+        'Adventure Travel', 
+        'City Tours', 
+        'Nature Trails', 
+        'Luxury Resorts', 
+        'Hidden Gems', 
+        'Specials '
+    ],
+    typeSpeed: 60,
+    backSpeed: 60,
+    backDelay: 1000,
+    loop: true,
+});
